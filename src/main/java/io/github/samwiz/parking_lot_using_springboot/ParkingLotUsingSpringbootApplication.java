50941aef989fc38d4e2acf65b3e59a3590053d60
Sam-wiz <samrudhvvandakudri2005@gmail.com>
2024-04-30 21:14:25 +0530

    Intialised spring-project

---
+++ src/main/java/io/github/samwiz/parking_lot_using_springboot/ParkingLotUsingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package io.github.samwiz.parking_lot_using_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotUsingSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingLotUsingSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/samwiz/parking_lot_using_springboot/ParkingLotUsingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.samwiz.parking_lot_using_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotUsingSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
